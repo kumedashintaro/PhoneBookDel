bcc2590745e0a99833cbfa893f9fb72d5fd29106
Kumeda Shintaro <pii.k2.bys@gmail.com>
2021-02-04 21:41:05 +0900

    Initial Commit

---
+++ phoneBookDel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  phoneBookDel
+//
+//  Created by 久米田晋太郎 on 2021/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
