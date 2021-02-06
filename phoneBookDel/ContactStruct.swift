a19cabaeab5a582e736f92dad4daaba9d1855a6a
Kumeda Shintaro <pii.k2.bys@gmail.com>
2021-02-06 18:23:08 +0900

    電話帳表示を実装

---
+++ phoneBookDel/ContactStruct.swift
@@ -0,0 +1,14 @@
+//
+//  ContactStruct.swift
+//  phoneBookDel
+//
+//  Created by 久米田晋太郎 on 2021/02/06.
+//
+
+import Foundation
+
+struct ContactStruct {
+    let givenName: String
+    let familyName: String
+    let number: String
+}
--- phoneBookDel/ViewController.swift
+++ phoneBookDel/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import Contacts
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet var tableView: UITableView!
+    
+    var contactStore = CNContactStore()
+    var contacts = [ContactStruct]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        contactStore.requestAccess(for: .contacts) { (success, error) in
+            if success {
+                print("Authorization Successfull")
+            }
+        }
+        
+        fetchContacts()
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
+        let contactToDisplay = contacts[indexPath.row]
+        cell.textLabel?.text = contactToDisplay.givenName + " " + contactToDisplay.familyName
+        cell.detailTextLabel?.text = contactToDisplay.number
+        return cell        
+    }
+    
+    
+    func fetchContacts() {
+        
+        let key = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactPhoneNumbersKey] as [CNKeyDescriptor]
+        let request = CNContactFetchRequest(keysToFetch: key)
+        try! contactStore.enumerateContacts(with: request) { (contact, stoppingPointer) in
+            
+            let name = contact.givenName
+            let familyName = contact.familyName
+            let number = contact.phoneNumbers.first?.value.stringValue
+            
+            if (number == nil){
+                let contactToAppend = ContactStruct(givenName: name, familyName: familyName, number: "番号未登録")
+                self.contacts.append(contactToAppend)
+                
+            } else {
+                
+                let contactToAppend = ContactStruct(givenName: name, familyName: familyName, number: number!)
+                self.contacts.append(contactToAppend)
+            }
+        }
+        tableView.reloadData()
+    }
+    
 }
 
